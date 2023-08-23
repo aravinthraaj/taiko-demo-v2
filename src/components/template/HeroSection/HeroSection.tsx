ac899bf1d7d35e45f6efaa637d4f18415ae86d5f
Aravinth Raaj K <aravinthraajk@Aravinths-MacBook-Pro.local>
2023-08-23 14:53:42 +0530

    next images qualityupdated

--- src/components/template/HeroSection/HeroSection.tsx
+++ src/components/template/HeroSection/HeroSection.tsx
@@ -1,7 +1,7 @@
 import React from "react"
-import heroImage1 from "@Images/hero4.png"
-import heroImage2 from "@Images/hero5.png"
-import heroImage3 from "@Images/hero6.jpeg"
+import heroImage1 from "@Images/hero.webp"
+import heroImage2 from "@Images/hero2.webp"
+import heroImage3 from "@Images/hero3.webp"
 
 import styles from "./HeroSection.module.scss"
 import Logo from "@/components/atoms/Logo/Logo"
